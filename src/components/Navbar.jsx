4a255b699dbabe45fe1eec94e0e7be602f24e957
Stchristopher22 <stchristopher902@gmail.com>
2025-01-09 16:47:20 +0530

    part-5

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,41 @@
-import logo from "../assets/kevinRushLogo.png";
-import { FaLinkedin} from "react-icons/fa";
-import { FaGithub} from "react-icons/fa";
-import { FaInstagram} from "react-icons/fa";
-
-
+import { FaLinkedin } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
 
 const Navbar = () => {
   return (
     <nav className="mb-20 flex items-center justify-between py-6">
-    <div className="flex flex-shrink-0 items-center">
-  <img className="mx-5 w-10" src="{logo}" alt="logo" />
-    </div>
-    <div className="m-8 flex iteams-center justify-center gap-4 text-2xl">
-        <FaLinkedin />
-        <FaGithub />
-        <FaInstagram />
-    </div>
- </nav>
-   );
+      <div className="flex flex-shrink-0 items-center">
+        <h1 className="my-20 text-center font-sm tracking-tight text-4xl">ST</h1>
+      </div>
+      <div className="m-8 flex items-center justify-center gap-4 text-2xl">
+        <a
+          href="https://www.linkedin.com/in/christopher-st-564449225/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="hover:text-blue-600"
+        >
+          <FaLinkedin />
+        </a>
+        <a
+          href="https://github.com/Stchristopher22"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="hover:text-gray-600"
+        >
+          <FaGithub />
+        </a>
+        <a
+          href="https://instagram.com/christo__as__kitcha"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="hover:text-pink-600"
+        >
+          <FaInstagram />
+        </a>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
